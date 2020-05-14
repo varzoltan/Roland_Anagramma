5a7b3679b3d2f2753740ec68943b94e9ca61ee76
varzoltan <64203391+varzoltan@users.noreply.github.com>
2020-05-14 14:40:18 +0200

    Initial commit

---
+++ Roland_Anagramma/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Roland_Anagramma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.feladat
+            Console.Write("Kérem adjon meg egy szót: ");
+            string szo = Console.ReadLine();
+            char[] kar_tomb = szo.ToCharArray();
+            /*Array.Sort(kar_tomb);
+            for (int i = 0; i<kar_tomb.Length;i++)
+            {
+                Console.Write(kar_tomb[i]);              
+            }
+            Console.WriteLine();
+            int darabszam = 1;
+            for (int i = 1;i<szo.Length;i++)
+            {
+                if(kar_tomb[i] == kar_tomb[i-1])
+                {
+
+                    darabszam++;
+                    if (i == szo.Length - 1)
+                    {
+                        Console.WriteLine(kar_tomb[i] + ":" + darabszam);
+                    }
+                }
+
+                else 
+                {
+                    Console.WriteLine(kar_tomb[i - 1]+ ":"+darabszam);
+                    darabszam = 1;
+                    if (i == szo.Length - 1)
+                    {
+                        Console.WriteLine(kar_tomb[i] + ":" + darabszam);
+                    }
+                }
+            }*/
+            
+            for (int i = 'a';i<='z';i++)
+            {
+                int darabszam = 0;
+                for (int j = 0;j<kar_tomb.Length;j++)
+                {
+                    if (Convert.ToChar(i) == kar_tomb[j])
+                    {
+                        darabszam++;
+                    }                  
+                }
+                if (darabszam != 0)
+                {
+                    Console.WriteLine(Convert.ToChar(i) + ":" + darabszam);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
